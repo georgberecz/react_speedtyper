0d1560993534a512cc5a366092682a2d2a8f289d
georgberecz <georg.berecz@student.hpi.uni-potsdam.de>
2016-05-05 03:13:29 +0300

    complete about section

--- components/About.js
+++ components/About.js
@@ -8,7 +8,11 @@ module.exports = About = React.createClass({
 		return(
 			<div>
 				<p>Made by Georg Berecz. </p>
-				<p>To play press blub...</p>
+				<p>To play simply press the "Fetch Words" button and afterwards the "Start" button. <br />
+				By clicking "Start" the timer starts to count the seconds and you can directly begin to type the shown words. <br /> 
+				Hit SPACE if you completed the current word. It is not possible to delete typed characters! <br />
+				If you want to stop the game click the "Stop" button and your current scores are saved.
+				 </p>
 			</div>
 		);
 	}
